9912e62ca8697c53e456b7b9ab46e147fae9f052
Pamela Frieze <pfrieze@gmail.com>
2018-06-03 02:49:22 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
-require 'rake'
-require 'active_record'
-require 'yaml/store'
-require 'ostruct'
-require 'date'
+#require 'rake'
+#require 'active_record'
+#require 'yaml/store'
+#require 'ostruct'
+#require 'date'
 
 
 require 'bundler/setup'
